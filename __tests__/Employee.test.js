f9b7839c1f5b1ee8f528b2982e5bbd3d237f5c39
Ishya Casukhela <casukhelai@gmail.com>
2021-07-10 17:04:04 -0400

    created employee test, and added skeleton testing code

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,46 @@
+// install and run jest & inquirer
+
+const Employee = require('../lib/Employee');
+
+// test every name, id, email, getName(), getId(), getEmail(), getRole()
+describe("Employee", () => {
+    // name
+    it("", () => {
+        const employee = new Employee();
+        expect(typeof(e)).toBe("object");
+    });
+
+    // id
+    it("", () => {
+
+    });
+
+    // email
+    it("", () => {
+
+    });
+
+});
+
+    // getName()
+    describe("getName", () => {
+        it("", () => {
+
+        });
+    });
+
+    // getId()
+    it("getId", () => {
+
+    });
+
+    // getEmail()
+    it("getEmail", () => {
+
+    });
+
+    // getRole()
+    it("getRole", () => {
+
+    });
+
