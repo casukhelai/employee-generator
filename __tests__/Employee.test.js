264e7993af0960132c7a28753cf2c1bf8cffd0f5
Ishya Casukhela <casukhelai@gmail.com>
2021-07-10 18:02:32 -0400

    made each method have its own describe/it statements

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,35 +2,44 @@
 
 const Employee = require('../lib/Employee');
 
-// test every name, id, email, getName(), getId(), getEmail(), getRole()
+// test every name [getName()], id [getId()], email [getEmail()], & getRole()
 describe("Employee", () => {
-    // name
-    it("getName() should return employee name", () => {
-        const name = "Henry Spencer"
-        const employee = new Employee(name);
-        expect(employee.name).toBe(name);
-    });
+    describe("Employee Name", () => {
+        // name
+        it("getName() should return employee name", () => {
+            const name = "Henry Spencer";
+            const employee = new Employee(name);
+            expect(employee.name).toBe(name);
+        });
 
-    // id
-    it("getID() should return the employee ID number", () => {
-        const id = "1";
-        const employee = new Employee("Henry Spencer", id, "henry@psych.com");
-        expect(employee.id).toBe(id);
+    describe("Employee ID", () => {
+        // id
+        it("getID() should return the employee ID number", () => {
+            const id = "1";
+            const employee = new Employee("Henry Spencer", id, "henry@psych.com");
+            expect(employee.id).toBe(id);
+        });
     });
-
-    // email
-    it("getEmail() should return employee email", () => {
-        const email = "henry@psych.com";
-        const employee = new Employee("Henry Spencer", 1, email);
-        expect(employee.getEmail()).toBe(email);
+    
+    describe("Employee Email", () => {
+        // email
+        it("getEmail() should return employee email", () => {
+            const email = "henry@psych.com";
+            const employee = new Employee("Henry Spencer", 1, email);
+            expect(employee.getEmail()).toBe(email);
+        });
     });
- 
-    it("getRole() should return 'employee' ", () => {
-        const role = "Employee";
-        const employee = new Employee("Henry Spencer", 1, "henry@psych.com");
-        expect(employee.getRole()).toBe(role);
+    
+    describe("Employee Role", () => {
+        // getRole() test
+        it("getRole() should return 'employee' ", () => {
+            const role = "Employee";
+            const employee = new Employee("Henry Spencer", 1, "henry@psych.com");
+            expect(employee.getRole()).toBe(role);
+        });
     });
-
+    
+    });
 });
 
 
